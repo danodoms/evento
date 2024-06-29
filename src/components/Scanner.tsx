1e48ca64d3c0a0e556c8962a9b3b2443a4231d95
danodoms <danodoms@gmail.com>
2024-06-29 16:56:54 +0800

    removed useeffect in scanner.tsx

--- src/components/Scanner.tsx
+++ src/components/Scanner.tsx
@@ -1,99 +1,81 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Html5QrcodeScanner } from 'html5-qrcode';
-import { StudentMissingModal } from '@/components/StudentMissingModal'; // Import your ScanSuccessModal component
+"use client"
+
+import React, { useRef, useState } from 'react';
+import { Html5QrcodeScanner, Html5QrcodeResult } from 'html5-qrcode';
+import { StudentMissingModal } from '@/components/StudentMissingModal';
 import { Student } from '@/models/Student';
-import { toast } from "sonner";
 import { handleAttendanceRecord } from '@/models/Attendance';
-import { getStudentByIdNum, getAllStudents } from '@/models/Student';
+import { getStudentByIdNum } from '@/models/Student';
 
-const Scanner = () => {
-    const scannerRef = useRef<HTMLDivElement>(null);
+interface ModalContent {
+    desc: string;
+    subtitle: string;
+}
+
+const Scanner: React.FC = () => {
     const html5QrcodeScannerRef = useRef<Html5QrcodeScanner | null>(null);
-    const [isScanning, setIsScanning] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [successModalDesc, setSuccessModalDesc] = useState("Lorem ipsum");
-    const [successModalSubtitle, setSuccessModalSubtitle] = useState("Lorem ipsum");
+    const [modalContent, setModalContent] = useState<ModalContent>({ desc: "", subtitle: "" });
 
+    const successSound = new Audio('/success.mp3');
+    const failSound = new Audio('/fail.mp3');
 
-    useEffect(() => {
-        const successSound = new Audio('/success.mp3');
-        const failSound = new Audio('/fail.mp3');
+    const onScanSuccess = async (decodedText: string, decodedResult: Html5QrcodeResult) => {
+        try {
+            const student: Student | null = await getStudentByIdNum(decodedText);
 
-
-        const onScanSuccess = async (decodedText: string, decodedResult: any) => {
-            console.log(`Scanned Value = ${decodedText}`, decodedResult);
-
-
-            try {
-                // const userDetails: Student | null = await getStudentDetailsByIdNum(decodedText);
-                const student: Student | null = await getStudentByIdNum(decodedText);
-                getAllStudents();
-
-                if (student) {
-                    setSuccessModalDesc(`ID: ${decodedText}`);
-                    setSuccessModalSubtitle(student.name);
-                    setIsModalOpen(true);
-                    successSound.play();
-                    handleAttendanceRecord(student.id)
-                } else {
-                    html5QrcodeScannerRef.current?.pause();
-                    setSuccessModalDesc("The scanned ID does not match any user.");
-                    setSuccessModalSubtitle(`Scanned ID: ${decodedText}`);
-                    failSound.play();
-                    setIsModalOpen(true);
-                }
-            } catch (error) {
-                console.error("Error fetching student details: ", error);
-                html5QrcodeScannerRef.current?.pause();
-                setSuccessModalDesc("An error occurred while fetching student details.");
-                setSuccessModalSubtitle(`Scanned ID: ${decodedText}`);
+            if (student) {
+                setModalContent({ desc: `ID: ${decodedText}`, subtitle: student.name });
+                successSound.play();
+                handleAttendanceRecord(student.id);
+            } else {
+                setModalContent({ desc: "The scanned ID does not match any user.", subtitle: `Scanned ID: ${decodedText}` });
                 failSound.play();
-                setIsModalOpen(true);
             }
 
-            // Pause the scanner for 500ms after successful scan
-            if (html5QrcodeScannerRef.current) {
-                html5QrcodeScannerRef.current.pause();
-                setTimeout(() => {
-                    html5QrcodeScannerRef.current?.resume();
-                }, 500);
-            }
-        };
-
-        const onScanFailure = (error: any) => {
-            // console.warn(`QR Code scan error: ${error}`);
-        };
-
-        if (scannerRef.current) {
-            const html5QrcodeScanner = new Html5QrcodeScanner(
-                scannerRef.current.id,
-                { fps: 5, qrbox: { width: 250, height: 250 } },
-                /* verbose= */ false
-            );
-
-            html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-            html5QrcodeScannerRef.current = html5QrcodeScanner; // Save the scanner instance
-
-            // Indicate that scanning has started
-            setIsScanning(true);
-
-            return () => {
-                // Cleanup the scanner when the component is unmounted
-                html5QrcodeScanner.clear();
-                setIsScanning(false);
-            };
+            setIsModalOpen(true);
+            html5QrcodeScannerRef.current?.pause();
+            setTimeout(() => html5QrcodeScannerRef.current?.resume(), 500);
+        } catch (error) {
+            console.error("Error fetching student details:", error);
+            setModalContent({ desc: "An error occurred while fetching student details.", subtitle: `Scanned ID: ${decodedText}` });
+            failSound.play();
+            setIsModalOpen(true);
+            html5QrcodeScannerRef.current?.pause();
         }
-    }, []);
+    };
+
+    const onScanFailure = (error: any) => {
+        // console.warn(`QR Code scan error: ${error}`);
+    };
 
     const handleCloseModal = () => {
         setIsModalOpen(false);
-        html5QrcodeScannerRef.current?.resume()
+        html5QrcodeScannerRef.current?.resume();
+    };
+
+    const setScannerRef = (node: HTMLDivElement | null) => {
+        if (node && !html5QrcodeScannerRef.current) {
+            const html5QrcodeScanner = new Html5QrcodeScanner(
+                node.id,
+                { fps: 5, qrbox: { width: 250, height: 250 } },
+                false
+            );
+
+            html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+            html5QrcodeScannerRef.current = html5QrcodeScanner;
+        }
     };
 
     return (
         <div className="flex flex-col items-center justify-center">
-            <div id="reader" ref={scannerRef} className="w-full max-w-sm rounded"></div>
-            <StudentMissingModal subtitle={successModalSubtitle} description={successModalDesc} isOpen={isModalOpen} onClose={handleCloseModal} />
+            <div id="reader" ref={setScannerRef} className="w-full max-w-sm rounded"></div>
+            <StudentMissingModal
+                subtitle={modalContent.subtitle}
+                description={modalContent.desc}
+                isOpen={isModalOpen}
+                onClose={handleCloseModal}
+            />
         </div>
     );
 };
