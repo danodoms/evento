c2c0b0ffe444719940eb16fda5047a574203e53a
danodoms <danodoms@gmail.com>
2024-07-08 10:15:00 +0800

    added event filters

--- src/app/events/create/page.tsx
+++ src/app/events/create/page.tsx
@@ -2,12 +2,20 @@ import { EventForm } from "../EventForm";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
+import Link from "next/link"
+import { Calendar } from "lucide-react";
+
+
 export default function RegisterStudentPage() {
     return (
         <div className="flex flex-col gap-4">
-            <h1 className="text-3xl font-bold tracking-tight sm:text-5xl">
-                Add Event
-            </h1>
+            <div className="flex justify-between items-center">
+                <h1 className="text-2xl font-bold tracking-tight">Add Event</h1>
+                <Link href="/events" className="flex gap-2 items-center button font-semibold border p-2 rounded-lg">
+                    <Calendar className="mr size-4" />View Events
+                </Link>
+            </div>
+
             <div className="gap-y-6">
                 <EventForm />
             </div>
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -26,10 +26,24 @@ import {
 // import { Switch } from "@/components/ui/switch"
 import { Badge } from "@/components/ui/badge";
 
-import { format, parseISO } from 'date-fns';
+import { format, parseISO, getMonth, getYear } from 'date-fns';
 import { MapPin, Clock, Calendar, Pencil, Trash } from "lucide-react";
 
 import Link from "next/link"
+
+import { useState, useMemo } from 'react';
+
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
 
 
 
@@ -55,6 +69,24 @@ export default function EventsPage() {
         return formattedDate;
     }
 
+    const [filter, setFilter] = useState<string>('');
+    const [durationFilter, setDurationFilter] = useState<string>('');
+    const [monthFilter, setMonthFilter] = useState<number | null>(null);
+    const [yearFilter, setYearFilter] = useState<number | null>(null);
+
+
+
+    const uniqueMonths = useMemo(() => {
+        const months = events.map(event => getMonth(parseISO(event.date)));
+        return Array.from(new Set(months));
+    }, [events]);
+
+    const uniqueYears = useMemo(() => {
+        const years = events.map(event => getYear(parseISO(event.date)));
+        console.log("Unique Years", Array.from(new Set(years)));
+        return Array.from(new Set(years));
+    }, [events]);
+
     function renderEventDuration(duration: Event["duration"]): string {
         switch (duration) {
             case "AM_ONLY":
@@ -68,6 +100,16 @@ export default function EventsPage() {
         }
     }
 
+    const filteredEvents = events.filter(event => {
+        const eventDate = parseISO(event.date);
+        return (
+            event.name.toLowerCase().includes(filter.toLowerCase()) &&
+            (durationFilter ? event.duration === durationFilter : true) &&
+            (monthFilter !== null ? getMonth(eventDate) === monthFilter : true) &&
+            (yearFilter !== null ? getYear(eventDate) === yearFilter : true)
+        );
+    });
+
     return (
         <div className="flex flex-col gap-4">
             <div className="flex justify-between items-center">
@@ -77,37 +119,133 @@ export default function EventsPage() {
                 </Link>
             </div>
 
-            <div className="flex flex-col gap-4">
-                {events.map((event, index) => (
-                    <div key={index} className="p-4 border rounded-lg flex flex-col gap-2">
-                        <div className="flex justify-between items-center">
-                            <div className="flex gap-2 items-center">
-                                <Calendar className="size-5" />
-                                <p className="font-bold text-sm">{formatDate(event.date)}</p>
-                            </div>
-                            <div className="flex gap-2 items-center">
-                                <Link href="#" className="text-xs border p-2 rounded-full flex gap-1"><Trash className="size-4" /></Link>
-                                <Link href="#" className="text-xs border p-2 rounded-full flex gap-1"><Pencil className="size-4" /></Link>
-                            </div>
-                        </div>
 
-                        <Separator className="my-1" />
 
-                        <h2 className="font-bold text-xl">{event.name}</h2>
 
-                        <div className="text-xs text-balance truncate">
-                            {event.description ? event.description : "No description"}
-                        </div>
 
-                        <div className="flex gap-2 flex-wrap mt-1">
-                            <Badge className="flex gap-1"><Clock className="size-3" />{renderEventDuration(event.duration)}</Badge>
-                            {event.location && (
-                                <Badge className="flex gap-1"><MapPin className="size-3" />{event.location}</Badge>
-                            )}
-                        </div>
-                    </div>
-                ))}
+
+
+
+
+
+            <div className="flex gap-2 justify-evenly">
+                {/* <input
+                    type="text"
+                    placeholder="Filter by name"
+                    value={filter}
+                    onChange={e => setFilter(e.target.value)}
+                    className="p-2 border rounded-lg"
+                /> */}
+
+
+
+                <Select onValueChange={value => setYearFilter(value ? Number(value) : null)} value={yearFilter !== null ? yearFilter.toString() : ''}>
+                    <SelectTrigger className="">
+                        <SelectValue placeholder="All Years" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Years</SelectLabel>
+                            <SelectItem value={null}>All Years</SelectItem>
+                            {uniqueYears.map(year => (
+                                <SelectItem key={year} value={year.toString()}>{year}</SelectItem>
+                            ))}
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+
+
+
+
+                <Select onValueChange={value => setMonthFilter(value ? Number(value) : null)} value={monthFilter !== null ? monthFilter.toString() : ''}>
+                    <SelectTrigger className="">
+                        <SelectValue placeholder="All Months" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Months</SelectLabel>
+                            <SelectItem value={null}>All Months</SelectItem>
+                            {uniqueMonths.map(month => (
+                                <SelectItem key={month} value={month.toString()}>{format(new Date(0, month), "MMMM")}</SelectItem>
+                            ))}
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+
+
+
+
+
+                <Select onValueChange={value => setDurationFilter(value)} value={durationFilter}>
+                    <SelectTrigger className="">
+                        <SelectValue className="text-xs" placeholder="All Durations" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Durations</SelectLabel>
+                            <SelectItem value={null}>All Durations</SelectItem>
+                            <SelectItem value="AM_ONLY">Morning</SelectItem>
+                            <SelectItem value="PM_ONLY">Afternoon</SelectItem>
+                            <SelectItem value="AM_AND_PM">Whole Day</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+
             </div>
+
+
+
+
+
+
+
+
+
+
+
+
+            {filteredEvents ? (
+                <div className="flex flex-col gap-4">
+                    {/* RENDER EVENT CARDS */}
+                    {/* RENDER EVENT CARDS */}
+                    {/* RENDER EVENT CARDS */}
+                    {filteredEvents.map((event, index) => (
+                        <div key={index} className="p-4 border rounded-lg flex flex-col gap-2">
+                            <div className="flex justify-between items-center">
+                                <div className="flex gap-2 items-center">
+                                    <Calendar className="size-5" />
+                                    <p className="font-bold text-sm">{formatDate(event.date)}</p>
+                                </div>
+                                <div className="flex gap-2 items-center">
+                                    <Link href="#" className="text-xs border p-2 rounded-full flex gap-1"><Trash className="size-4" /></Link>
+                                    <Link href="#" className="text-xs border p-2 rounded-full flex gap-1"><Pencil className="size-4" /></Link>
+                                </div>
+                            </div>
+
+                            <Separator className="my-1" />
+
+                            <h2 className="font-bold text-xl">{event.name}</h2>
+
+                            <div className="text-xs text-balance truncate">
+                                {event.description ? event.description : "No description"}
+                            </div>
+
+                            <div className="flex gap-2 flex-wrap mt-1">
+                                <Badge className="flex gap-1"><Clock className="size-3" />{renderEventDuration(event.duration)}</Badge>
+                                {event.location && (
+                                    <Badge className="flex gap-1"><MapPin className="size-3" />{event.location}</Badge>
+                                )}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div className="flex flex-col gap-4">
+                    <p>No events</p>
+                </div>
+            )}
+
+
 
             <ToastContainer />
         </div>
