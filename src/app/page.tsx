2c401e9dccf6bac333801066f1d452f9648a3a2f
danodoms <danodoms@gmail.com>
2024-07-15 22:13:40 +0800

    changed homepage design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,40 @@ import {
 	GanttChart,
 	GanttChartIcon,
 	Heart,
+	Scan,
 	UserRoundPlus,
 	UsersRound,
+	QrCode,
+	Map,
+	ArrowRight,
+	FlaskConical,
+	Circle,
+	Badge,
+	BadgeCheck,
+	CircleCheckBig
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import type React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card"
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel"
 
 
-import { Badge } from "@/components/ui/badge";
+// import { Badge } from "@/components/ui/badge";
 import AttendanceHistory from "./history/AttendanceHistory";
 
 interface UiButton {
@@ -43,6 +67,29 @@ const buttons: UiButton[] = [
 	},
 ];
 
+const features = [
+	{
+		name: "Attendance Tracking",
+		iconName: GanttChartIcon,
+		description: "Track attendance and participation",
+	},
+	{
+		name: "QR Code Scanning",
+		iconName: QrCode,
+		description: "Scan QR codes to mark attendance",
+	},
+	{
+		name: "Student Management",
+		iconName: UsersRound,
+		description: "Manage student information",
+	},
+	{
+		name: "Event Report Generation",
+		iconName: CalendarX,
+		description: "Export event attendance reports",
+	},
+];
+
 export default function Home() {
 	return (
 		<section className="flex flex-col gap-4 p-2">
@@ -58,9 +105,9 @@ export default function Home() {
 
 
 
-			<div className="flex flex-col gap-2 rounded-lg ">
-				<h2 className="text-2xl text-pretty  font-bold items-center gap-2">
-					Welcome to {appName}
+			<div className="flex flex-col gap-2 rounded-lg -">
+				<h2 className="text-4xl text-pretty  font-bold items-center gap-2">
+					Home
 					{/* <span className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-blue-500">
 						{" "}
 						{appName}
@@ -92,7 +139,9 @@ export default function Home() {
 				</div>
 			</div>
 
-			<div className="flex flex-col gap-2 p-4 border rounded-lg">
+
+
+			{/* <div className="flex flex-col gap-2 p-4 border rounded-lg">
 				<div className="font-semibold text-md text-pretty flex items-center gap-2">
 					Dashboard
 				</div>
@@ -102,7 +151,44 @@ export default function Home() {
 
 					<CalendarX />
 				</div>
+			</div> */}
+
+			<div className="font-semibold text-pretty flex items-center gap-2">
+				<Map />
+				<span>Explore Features</span>
 			</div>
+
+			<Carousel className="w-full ">
+				<CarouselContent>
+					{features.map((feature, index) => (
+						<CarouselItem key={index}>
+
+							<Card className="bg-primary opacity-95 text-background ">
+								<CardHeader>
+									<CardTitle className="flex gap-2 items-center">{feature.name} 	<CircleCheckBig className="size-5 " /></CardTitle>
+									<CardDescription>{feature.description}</CardDescription>
+
+								</CardHeader>
+								{/* <CardContent>
+
+								</CardContent> */}
+								<CardFooter className="flex gap-1 items-center opacity-50 ">
+									{/* <Button className="w-full">
+										<Check className="mr-2 h-4 w-4" /> Mark all as read
+									</Button> */}
+									<p className="text-xs">Swipe right to reveal more features </p>
+									<ArrowRight className="size-4" />
+								</CardFooter>
+							</Card>
+
+						</CarouselItem>
+					))}
+				</CarouselContent>
+				{/* <CarouselPrevious />
+				<CarouselNext /> */}
+			</Carousel>
+
+
 
 			{/* BOTTOM SECTION */}
 			{/* <div className="gap-4 flex flex-col grid-cols-2 md:grid">
@@ -117,13 +203,15 @@ export default function Home() {
         ))}
       </div> */}
 
-			<div className="font-semibold text-md text-pretty flex items-center gap-2">
-				Recent Scan Results
+			<div className="font-semibold text-pretty flex items-center gap-2">
+				<QrCode />
+
+				<span>Recent Scan Results</span>
 			</div>
 
 			<div className="overflow-auto h-80 rounded-lg">
 				<AttendanceHistory />
 			</div>
-		</section>
+		</section >
 	);
 }
