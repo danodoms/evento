91a9f12fa510c394b222f2abccb2faacbc1905f5
danodoms <danodoms@gmail.com>
2024-08-04 09:54:45 +0800

    updated portal ui

--- apps/portal/src/app/page.tsx
+++ apps/portal/src/app/page.tsx
@@ -3,6 +3,8 @@ import { HeroSection } from "@/components/HeroSection"
 import { FeaturesSection } from "@/components/FeaturesSection"
 import { CTASection } from "@/components/CTASection"
 import { Footer } from "@/components/Footer"
+import { Content1 } from "@/components/Content1"
+import { Content2 } from "@/components/Content2"
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
 
       <main className="flex-1">
         <HeroSection />
+        <Content1 />
+        <Content2 />
         <FeaturesSection />
         {/* <CTASection /> */}
       </main>
---
+++ apps/portal/src/components/DevInfo.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+
+import { Briefcase, CalendarDays, Download, Map, TriangleAlert } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+
+import { motion } from "framer-motion"
+
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card"
+
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+export default function DevInfo() {
+    return (
+        <HoverCard>
+            <HoverCardTrigger asChild>
+                <div className="flex text-sm font-semibold pl-2 pr-3 py-2 rounded-full items-center ">
+
+                    <div className='flex gap-2 items-center font-semibold'>
+                        <Avatar className='size-8'>
+                            <AvatarImage src="https://avatars.githubusercontent.com/u/165539900?v=4" />
+                            <AvatarFallback>DD</AvatarFallback>
+                        </Avatar>
+
+                        <p>developed by @danodoms</p>
+                    </div>
+                </div>
+            </HoverCardTrigger>
+            <HoverCardContent className="w-80">
+                <div className="flex justify-between space-x-4">
+                    <Avatar>
+                        <AvatarImage src="https://avatars.githubusercontent.com/u/165539900?v=4" />
+                        <AvatarFallback>VC</AvatarFallback>
+                    </Avatar>
+                    <div className="space-y-1">
+                        <h4 className="text-sm font-semibold">@danodoms</h4>
+                        <p className="text-sm">
+                            I make things for the web.
+                        </p>
+
+                        <a href="https://danodoms.vercel.app" target='_blank' className=' underline italic font-normal text-xs flex gap-2 items-center'>
+
+                            <Briefcase className='size-4' />
+
+                            My Portfolio</a>
+
+                    </div>
+                </div>
+            </HoverCardContent>
+        </HoverCard>
+    )
+}
